96ff6569c8d9b053cf7a3e137e32f979a9e2d11e
Bihan-Siriwardhana <155979347+Bihan-Siriwardhana@users.noreply.github.com>
2025-03-14 08:21:21 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/mit/rma_web_application/RmaWebApplication.java
@@ -0,0 +1,13 @@
+package com.mit.rma_web_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmaWebApplication.class, args);
+	}
+
+}
