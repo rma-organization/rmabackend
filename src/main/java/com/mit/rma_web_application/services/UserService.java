d131081e5a4608370200577236a8861c04630c16
Bihan-Siriwardhana <155979347+Bihan-Siriwardhana@users.noreply.github.com>
2025-07-20 17:33:30 +0530

    remove things

--- src/main/java/com/mit/rma_web_application/services/UserService.java
+++ src/main/java/com/mit/rma_web_application/services/UserService.java
@@ -1,22 +1,4 @@
-//
-//
-//package com.mit.rma_web_application.services;
-//
-//import com.mit.rma_web_application.dtos.RegisterRequestDTO;
-//import com.mit.rma_web_application.models.User;
-//
-//import java.util.List;
-//
-//public interface UserService {
-//    User registerUser(RegisterRequestDTO registrationDto);
-//    boolean existsByUsername(String username);
-//    User findByUsername(String username);
-//    List<User> getPendingUsers();
-//    User approveUser(Long userId);
-//    String generateToken(String username);
-//    User save(User user);
-//    List<User> getAllUsers();
-//}
+
 
 package com.mit.rma_web_application.services;
 
